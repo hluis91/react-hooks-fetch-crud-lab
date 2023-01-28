27a78e45b304ee937b3be04c2c69ada9f5b87860
Luis Hernandez <lshrnndz6@gmail.com>
2023-01-28 15:27:05 -0800

    Lab Done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleQuizDelete, handleQuizUpdate }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,23 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDeleteQuizClick() {
+    handleQuizDelete(question)
+  }
+
+  function handleOptionChange(event) {
+    handleQuizUpdate(question, event.target.value)
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleOptionChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteQuizClick}>Delete Question</button>
     </li>
   );
 }
